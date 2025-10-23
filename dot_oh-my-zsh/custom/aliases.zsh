2d1ec9dde7ce5232e84e07b08caa1fe4d7552fe3
Brandon Scott <contact@brandonscott.me>
2025-10-23 19:31:33 -0400

    chore: port over show/hide dot file functions and alias them for greppability

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,4 +1,17 @@
 alias gs='git status'
 alias openpr='gh pr create --web'
+alias reload='omz reload'
 alias sourceme='omz reload'
+function hidedotfiles() {
+	defaults write com.apple.finder AppleShowAllFiles NO
+	killall Finder /System/Library/CoreServices/Finder.app
+	echo "Dot files are now hidden. You can also toggle hidden files in a Finder window with ⌘ + SHIFT + ."
+}
+function showdotfiles() {
+	defaults write com.apple.finder AppleShowAllFiles YES
+	killall Finder /System/Library/CoreServices/Finder.app
+	echo "Dot files are now shown. You can also toggle hidden files in a Finder window with ⌘ + SHIFT + ."
+}
+alias showhiddenfiles='showdotfiles'
+alias hidehiddenfiles='hidedotfiles'
 
